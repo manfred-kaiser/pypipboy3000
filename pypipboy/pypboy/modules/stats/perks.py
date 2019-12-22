19bfb738a878998d8fbfb5ea43969f1e9661d2c6
Manfred Kaiser <manfred.kaiser@logfile.at>
2019-12-22 20:46:47 +0100

    cleaned modules

--- pypipboy/pypboy/modules/stats/perks.py
+++ pypipboy/pypboy/modules/stats/perks.py
@@ -6,6 +6,3 @@ class Module(pypboy.SubModule):
     LABEL = "Perks"
     headline = "STATUS"
     title = " HP 160/175  |  AP 62/62"
-
-    def __init__(self, *args, **kwargs):
-        super(Module, self).__init__(*args, **kwargs)
